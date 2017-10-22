ea0ce173deac41092ebc975cbefbacffb57ed334
Johanna Merikanto <johanna.merikanto@stud.pointcollege.fi>
2017-10-22 18:56:17 +0300

    Luotu uusi Angular sovellus. Ei vielä lisätty koodeja.

---
+++ AlytaloAngularDemo/AlytaloAngularDemo/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AlytaloAngularDemo.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ AlytaloAngularDemo/AlytaloAngularDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AlytaloAngularDemo.WebApiApplication" Language="C#" %>
